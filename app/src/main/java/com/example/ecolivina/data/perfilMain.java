c47ad2a7ee150a6f3454b99a32f965113db64d78
doramaslemes <doramaslemes@gmail.com>
2023-03-02 00:14:38 +0000

    Avance navegation bar y actividad perfil

---
+++ app/src/main/java/com/example/ecolivina/data/perfilMain.java
@@ -0,0 +1,16 @@
+package com.example.ecolivina.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecolivina.R;
+
+public class perfilMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_main);
+    }
+}
