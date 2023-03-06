6aa3c77ceb2ec99407e11ad2e69ad8218ac2e64e
doramaslemes <doramaslemes@gmail.com>
2023-03-06 22:03:11 +0000

    Creacion de servicio para MySQL y añadir al ArrayList todos los valores de los productos, creacion del reciclerView y configuracion del mismo

---
+++ app/src/main/java/com/example/ecolivina/data/AdapterProductos.java
@@ -0,0 +1,65 @@
+package com.example.ecolivina.data;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ecolivina.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Creacion del adaptador para mostrar cada producto en el recycler view
+public class AdapterProductos extends RecyclerView.Adapter<AdapterProductos.ViewHolder>{
+
+    ArrayList<List<String>> listaProductos;
+
+    public AdapterProductos(ArrayList<List<String>> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    //Se infla el view holder
+    @NonNull
+    @Override
+    public AdapterProductos.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.lista_producto, parent, false);
+        return new ViewHolder(view);
+    }
+
+    //Comunica adaptador con viewHolder y se añaden los datos de cada producto
+    @Override
+    public void onBindViewHolder(@NonNull AdapterProductos.ViewHolder holder, int position) {
+        holder.asignarProductos(listaProductos.get(0).get(position), listaProductos.get(1).get(position), listaProductos.get(2).get(position));
+    }
+
+    //Devuelve el tamaño de la lista de productos
+    @Override
+    public int getItemCount() {
+        return listaProductos.size();
+    }
+
+    //Se crea el view holder para mostrar los datos de cada producto
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nombreProducto, precioProducto, pesoProducto;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nombreProducto=itemView.findViewById(R.id.nombreProducto);
+            precioProducto=itemView.findViewById(R.id.precioProducto);
+            pesoProducto=itemView.findViewById(R.id.pesoProducto);
+        }
+
+        //Se asignan los datos de cada producto
+        public void asignarProductos(String s, String s1, String s2) {
+            nombreProducto.setText(s);
+            precioProducto.setText(s1);
+            pesoProducto.setText(s2);
+        }
+    }
+}
--- app/src/main/java/com/example/ecolivina/data/MainActivity.java
+++ app/src/main/java/com/example/ecolivina/data/MainActivity.java
@@ -1,18 +1,61 @@
 package com.example.ecolivina.data;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.ecolivina.R;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<List<String>> listaProductos;
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Se crea el recycler view
+        recycler = (RecyclerView) findViewById(R.id.recyclerView);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        ejecutarServicio("http://10.0.2.2/ecolivina/consulta.php");
 
     }
-}
+
+    //Metodo buscar los productos de Ecolivina en MySQL
+    private void ejecutarServicio(String URL){
+        //Se crea la cola de peticiones, si la peticion es correcta se ejecuta el codigo de la funcion onResponse
+        JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(URL,response -> {
+            //Se crea la lista de productos
+            listaProductos = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject jsonObject = response.getJSONObject(i);
+                    listaProductos.add(new ArrayList<String>(Arrays.asList(jsonObject.getString("nombre"))));
+                    listaProductos.add(new ArrayList<String>(Arrays.asList(jsonObject.getString("precio"))));
+                    listaProductos.add(new ArrayList<String>(Arrays.asList(jsonObject.getString("peso"))));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error en la iteracion de los datos", Toast.LENGTH_SHORT).show();
+                }
+            }
+            //Se crea el adaptador
+            AdapterProductos adaptadorProductos = new AdapterProductos(listaProductos);
+            recycler.setAdapter(adaptadorProductos);
+        }, error -> {
+            Toast.makeText(MainActivity.this, "Error en la petición", Toast.LENGTH_SHORT).show();
+        });
+
+        };
+    }
