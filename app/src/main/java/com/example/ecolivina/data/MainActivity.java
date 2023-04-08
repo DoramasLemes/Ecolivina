f1617f37f77942455179e66929ffedacd63ba69d
doramaslemes <doramaslemes@gmail.com>
2023-04-08 17:07:00 +0100

    Creación del fragmento home y favs, añadido del reciclerView con los productos en el HomeFragment

--- app/src/main/java/com/example/ecolivina/data/MainActivity.java
+++ app/src/main/java/com/example/ecolivina/data/MainActivity.java
@@ -12,6 +12,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.ecolivina.R;
+import com.example.ecolivina.data.fragmentos.FavFragment;
+import com.example.ecolivina.data.fragmentos.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONObject;
@@ -24,11 +26,11 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<List<String>> listaProductos;
     RecyclerView recycler;
-
     String emailUser, passwordUser;
-
     //Declaracion del BottomMenu
     BottomNavigationView navigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    FavFragment favFragment = new FavFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +39,17 @@ public class MainActivity extends AppCompatActivity {
 
         navigationView= findViewById(R.id.navigationView);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
         //Se configura el listener para el bottom menu
         navigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
                 case R.id.navigation_home:
                     Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
                     break;
                 case R.id.navigation_favs:
                     Toast.makeText(MainActivity.this, "Favoritos", Toast.LENGTH_SHORT).show();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, favFragment).commit();
                     break;
                 case R.id.navigation_subir:
                     Toast.makeText(MainActivity.this, "Subir", Toast.LENGTH_SHORT).show();
@@ -68,11 +73,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(MainActivity.this, "El mail del usuario es: " +emailUser + "\n La contraseña del usuario es: " + passwordUser, Toast.LENGTH_SHORT).show();
 
-        //Se crea el recycler view
-        recycler = findViewById(R.id.recyclerView);
-        recycler.setLayoutManager(new LinearLayoutManager(this));
-        ejecutarServicio("http://10.0.2.2/ecolivina/productos.php");
-
     }
 
     //Metodo para cambiar de actividad
@@ -88,31 +88,4 @@ public class MainActivity extends AppCompatActivity {
         passwordUser = extras.getString("password");
         Toast.makeText(MainActivity.this, "El mail del usuario es: " +emailUser + "\n La contraseña del usuario es: " + passwordUser, Toast.LENGTH_SHORT).show();
     }
-
-    //Metodo buscar los productos de Ecolivina en MySQL
-    private void ejecutarServicio(String URL){
-        //Se crea la cola de peticiones, si la peticion es correcta se ejecuta el codigo de la funcion onResponse
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL,response -> {
-            //Se crea la lista de productos
-            listaProductos = new ArrayList<>();
-
-            //Se recorren los datos de la respuesta
-            for (int i = 0; i < response.length(); i++) {
-                try {
-                    JSONObject jsonObject = response.getJSONObject(i);
-                    listaProductos.add(new ArrayList<String>(Arrays.asList(jsonObject.getString("tipo"), jsonObject.getString("precio"), jsonObject.getString("peso"),jsonObject.getString("img"))));
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, "Error en la iteracion de los datos", Toast.LENGTH_SHORT).show();
-                }
-            }
-            //Se crea el adaptador
-            AdapterProductos adaptadorProductos = new AdapterProductos(listaProductos);
-            recycler.setAdapter(adaptadorProductos);
-        }, error -> {
-            Toast.makeText(MainActivity.this, "Error en la petición", Toast.LENGTH_SHORT).show();
-        });
-        //Se agrega la peticion a la cola
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsonArrayRequest);
-        };
-    }
+}
