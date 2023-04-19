433ae4fa944bc0c6c860528016b52c3ef4c45fbe
doramaslemes <doramaslemes@gmail.com>
2023-04-19 14:54:45 +0100

    Ajustes en el fragmento perfil y ciclo de vida para quitar el layout del main cuando se abre el perfil

--- app/src/main/java/com/example/ecolivina/data/MainActivity.java
+++ app/src/main/java/com/example/ecolivina/data/MainActivity.java
@@ -1,25 +1,22 @@
 package com.example.ecolivina.data;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.ecolivina.R;
 import com.example.ecolivina.data.fragmentos.FavFragment;
 import com.example.ecolivina.data.fragmentos.HomeFragment;
+import com.example.ecolivina.data.fragmentos.PerfilFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,36 +28,25 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
     HomeFragment homeFragment = new HomeFragment();
     FavFragment favFragment = new FavFragment();
+    PerfilFragment perfilFragment = new PerfilFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Se reciben los datos del usuario
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, perfilFragment);
+        //Se configura el bottom menu
         navigationView= findViewById(R.id.navigationView);
 
+        //Se configura el fragmento inicial
         getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
         //Se configura el listener para el bottom menu
         navigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case R.id.navigation_home:
-                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-                    break;
-                case R.id.navigation_favs:
-                    Toast.makeText(MainActivity.this, "Favoritos", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, favFragment).commit();
-                    break;
-                case R.id.navigation_subir:
-                    Toast.makeText(MainActivity.this, "Subir", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.navigation_buzon:
-                    Toast.makeText(MainActivity.this, "Buzon", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.navigation_perfil:
-                    cambiarActividad(perfilMain.class);
-                    break;
-            }
+            changeFragment(item.getItemId());
             return true;
         });
 
@@ -79,6 +65,27 @@ public class MainActivity extends AppCompatActivity {
     public void cambiarActividad(Class actividad){
         Intent intent = new Intent(this, actividad);
         startActivity(intent);
+    }
+
+    //Metodo para cambiar de fragmento
+    private void changeFragment(int id){
+        switch (id){
+            case R.id.navigation_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                break;
+            case R.id.navigation_favs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, favFragment).commit();
+                break;
+            case R.id.navigation_subir:
+                Toast.makeText(MainActivity.this, "Subir", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_buzon:
+                Toast.makeText(MainActivity.this, "Buzon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_perfil:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, perfilFragment).commit();
+                break;
+        }
     }
 
     //Metodo para recibir los datos del usuario
--- app/src/main/java/com/example/ecolivina/data/fragmentos/PerfilFragment.java
+++ app/src/main/java/com/example/ecolivina/data/fragmentos/PerfilFragment.java
@@ -2,19 +2,67 @@ package com.example.ecolivina.data.fragmentos;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ecolivina.R;
+import com.example.ecolivina.data.MainActivity;
 
 public class PerfilFragment extends Fragment {
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflar el layout para este fragmento
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
-    }
+        // Obtener la referencia al ConstraintLayout del main activity
+        MainActivity mainActivity = (MainActivity) getActivity();
+        assert mainActivity != null;
+        ConstraintLayout UILayout = mainActivity.findViewById(R.id.UILayout);
+
+        // Ocultamos el ConstraintLayout
+        UILayout.setVisibility(View.INVISIBLE);
+
+        // Cambiamos la altura del ConstraintLayout del main activity
+        ConstraintLayout.LayoutParams newParams = (ConstraintLayout.LayoutParams) UILayout.getLayoutParams();
+        newParams.height= 1;
+        UILayout.setLayoutParams(newParams);
+
+        // Agregar el LifecycleObserver al ciclo de vida del fragmento
+        getLifecycle().addObserver(lifecycleObserver);
+
+        return view;
+        }
+
+    // LifecycleObserver para el ciclo de vida del fragmento
+    private final LifecycleObserver lifecycleObserver = new LifecycleObserver() {
+        // Cuando el fragmento se destruye
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        public void onPaused() {
+            // Obtener la referencia al ConstraintLayout del main activity
+            MainActivity mainActivity = (MainActivity) getActivity();
+            ConstraintLayout UILayout;
+
+            {
+                assert mainActivity != null;
+                UILayout = mainActivity.findViewById(R.id.UILayout);
+            }
+            // Restablecer la restricción de ancho para los botones
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) UILayout.getLayoutParams();
+            UILayout.setVisibility(View.VISIBLE);
+            layoutParams.height = ConstraintLayout.LayoutParams.WRAP_CONTENT;
+            UILayout.setLayoutParams(layoutParams);
+        }
+    };
+
 }
